3ba82278c4b038d668413ce6766a5795a043a488
cupcicm <cupcicm@users.noreply.github.com>
2015-04-14 23:30:36 +0200

    Add an image
    And try to place text and images correctly.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,19 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
 var Vector2 = require('vector2');
 
 var wind = new UI.Window({ fullscreen: true });
 
 var text = new UI.Text({
-  position: new Vector2(47, 100),
-  size: new Vector2(97, 68),
+  position: new Vector2(42, 90),
+  size: new Vector2(65, 68),
  font: 'roboto-bold-subset-49',
  text: '05'
 });
+var image = new UI.Image({
+  position: new Vector2(47, 32),
+  size: new Vector2(50, 48),
+  image: 'images/notok.png'
+});
+wind.add(image);
 wind.add(text);
 wind.show();
-
